1603ea3efc0b6018997452d3992d050ddcdc1c3f
Heitor Pastorelli <heitorpessoal7@gmail.com>
2023-02-22 17:08:24 -0300

    Navbar css, accordion css, footer css, social media images added, google map added, appointment button css. All this only for mobile

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const hamburguerBtn = document.getElementsByClassName('hamburguer-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const accordion = document.getElementsByClassName('contentBx')
 
 hamburguerBtn.addEventListener('click', ()=>{
     navbarLinks.classList.toggle('active')
 })
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+}
